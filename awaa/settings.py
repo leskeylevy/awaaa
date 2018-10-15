e85b34ae7f3395df5093110610a12ece76d6d9c5
Levy <leskeylevy@gmail.com>
2018-10-15 07:04:07 +0300

    created profile model and its funtions and install tinymce

--- awaa/settings.py
+++ awaa/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+LOGIN_REDIRECT_URL = 'index'
 # Application definition
 
 INSTALLED_APPS = [
--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    dp = models.ImageField(upload_to='images')
+    bio = HTMLField(max_length=500)
+    contact = models.CharField(max_length=100)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return self.user.username
